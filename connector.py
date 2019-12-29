1db5ea1e83584c7098dcb413ff83fc822ab31669
Eian Lee <52366026+eian-lee@users.noreply.github.com>
2019-12-30 05:31:13 +0900

    payload에 uuid 설정
    일부 거래소는 payload에 고유한 uuid값을 요구하는 경우가 있어 추가함

--- connector.py
+++ connector.py
@@ -1,16 +1,19 @@
 
+import uuid
+import hashlib
 import logging
 import ujson as json
 import websocket
 import datetime
-from zmq_client import ZMQClient
+import zmq
 
-Log = logging.getLogger(__name__)
+LOG = logging.getLogger(__name__)
 
-class Connector(ZMQClient):
+class Connector(object):
     
-    def __init__(self, pairs, depth, **kwargs):
-        super().__init__()
+    id = "NotImplemented"
+    
+    def __init__(self, pairs, depth, ctx=None, addr=None, port=5555):
         self.pairs = pairs
         self.depth = depth
         self.base = self.pairs.split('/')[0]
@@ -20,28 +23,21 @@ class Connector(ZMQClient):
         self.payload = None
         self.name = self.__class__.__name__
         
-        # Websocket, ZMQ Socket 설정
+        # Payload에 고유값이 필요한 거래소설정
+        self.hash = str(uuid.uuid4)
+        self.uuid = self.id + self.hash
+        
+        # Websocket, ZMQ Socket 객체생성
         self.ws = None
-        self.is_connected = False
-        self.is_opened = False
+        self.ctx = ctx or zmq.Context()
+        self.addr = addr or "tcp://localhost:%s" % port
+        self.zsock = self.ctx.socket(zmq.REQ)
         self.zsock.connect(self.addr)
 
     def nonce(self):
-        return datetime.datetime.now().strftime('%H:%M:%S%f')
-
-    def connect(self):
-        self.ws = websocket.WebSocketApp(self.url,
-                                         on_open=self.on_open,
-                                         on_data=self.on_data,
-                                         on_error=self.on_error,
-                                         on_close=self.on_close)
-        self.ws.run_forever()
-        
-    def run(self):
-        self.connect()
-        
+        return datetime.datetime.now().strftime("%Y-%m-%d  %H:%M:%fS")
+    
     def on_open(self):
-        self.is_connected = True
         if self.ws.sock.connected:
             self.ws.send(json.dumps(self.payload))
     
@@ -49,18 +45,22 @@ class Connector(ZMQClient):
         return json.loads(data)
     
     def on_error(self, error):
-        Log.debug(f'{self.id}: {error}')
-        self.is_connected = False
-        if self.ws.sock:
-            self.ws.close()
-            self.zsock.close()
+        """[1900-01-01  00:00:0000] ExchangeName Error...
+        """
+        LOG.debug(f"[{self.nonce()}] {self.name} {error}")
     
     def on_close(self):
-        Log.info('Websocket has closed')
-        if self.is_connected:
-            self.is_connected = False
-            if self.ws.sock:
-                self.ws.close()
-                self.zsock.close()
-    
-    
+        LOG.info(f"[{self.nonce()}] Websocket Connection has been closed")
+
+    def connect(self):
+        self.ws = websocket.WebSocketApp(
+            self.url,
+            on_open=self.on_open,
+            on_data=self.on_data,
+            on_error=self.on_error,
+            on_close=self.on_close
+        )
+        self.ws.run_forever()
+        
+    def run(self):
+        self.connect()
